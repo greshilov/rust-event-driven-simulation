06e2db789d9f3f8b47097688e5374a5b6c4a2078
Viacheslav Greshilov <slovaricheg@gmail.com>
2021-04-26 03:54:18 +0300

    [red-server] Migrations for diesel cli

--- red-server/src/main.rs
+++ red-server/src/main.rs
@@ -14,7 +14,6 @@ pub mod views;
 
 use diesel::prelude::*;
 use dotenv::dotenv;
-use rocket_cors::AllowedOrigins;
 use std::env;
 
 pub fn establish_connection() -> PgConnection {
@@ -25,19 +24,7 @@ pub fn establish_connection() -> PgConnection {
 
 fn main() {
     dotenv().ok();
-
-    let allowed_origins = AllowedOrigins::some(
-        &["https://www.greshilov.me"],
-        &["^https://(.+).greshilov.me$"],
-    );
-    let cors = rocket_cors::CorsOptions {
-        allowed_origins,
-        ..Default::default()
-    }
-    .to_cors()
-    .unwrap();
     rocket::ignite()
         .mount("/reds", routes![views::top_scores, views::submit_scores])
-        .attach(cors)
         .launch();
 }
